94a7ffdaebfa91593037e52b6929a3495a2f9f11
Beth <bethhnielsen56@gmail.com>
2021-04-15 13:52:43 -0500

    wip

---
+++ big-O.js
@@ -0,0 +1,13 @@
+const nemo = ['nemo']
+const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
+const large = new Array(100000).fill('nemo');
+
+const findNemo = (array) => {
+  for (let index = 0; index < array.length; index++) {
+    if (array[i] === 'nemo') {
+      console.log('Found NEMO!')
+    }
+  }
+}
+
+findNemo(large);
